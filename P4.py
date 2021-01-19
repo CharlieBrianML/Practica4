f07b0c99d625e18877f5cb5823b546d7612c564c
Enrique Guerrero <chikehp710@yahoo.com>
2021-01-18 22:43:25 -0600

    Se agrega lectura de imagen de archivo binario

--- P4.py
+++ P4.py
@@ -1,1 +1,16 @@
-Practica4
+import matplotlib.pyplot as plt
+import numpy as np
+
+def read_img():
+    binaryFile = open("rawdata/rawdata/1223","rb")
+    data = np.fromfile(binaryFile, dtype='uint8') # reads the whole file
+    binaryFile.close()
+
+    print(data.shape)
+    data_reshaped = data.reshape(128,128)
+    plt.imshow(data_reshaped)
+    plt.show()
+
+
+if __name__ == "__main__":
+    read_img()
