c0bf3a5c1ae6e660998ce27ab0d338bb24da31d4
CharlieML <charls_brian@hotmail.es>
2021-01-20 06:59:23 -0600

    Se ha agragado promedio de las imagenes

--- P4.py
+++ P4.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+import cv2
 
 def read_img(fileName):
 	binaryFile = open('rawdata/'+fileName) # Lecura del archivo
@@ -9,12 +10,41 @@ def read_img(fileName):
 	binaryFile.close()
 	tam = int(np.sqrt(len(data))) # Se optiene el tamaño del la fila y columna
 	data_reshaped = data.reshape(tam,tam) # Se cambia la dimension del vector
-	plt.imsave('data/'+fileName+'.png',data_reshaped, cmap = 'gray') #
+	if(tam>128):
+		data_reshaped = cv2.resize(data_reshaped, (128,128), fx=0.75, fy=0.75) # Reescala la imagen a 128x128
+	return data_reshaped
 	
-def ls(ruta = 'rawdata'):
+def ls(ruta):
     return [arch for arch in listdir(ruta) if isfile(join(ruta, arch))] #Lista los archivos del directorio	
+	
+def vector_img(listFiles):
+	images = np.zeros((len(listFiles),128,128))
+	for i in range(len(listFiles)):
+		images[i,:,:] = cv2.imread('data/'+listFiles[i],0)
+	return images
+	
+def save_imges(listFiles):
+	for i in range(len(listFiles)):
+		img = read_img(listFiles[i])
+		plt.imsave('data/'+listFiles[i]+'.png',img, cmap = 'gray') # Guarda como imagen .png
+		
+def mean(matrix):
+	matrix_mean = np.zeros((128, 128))
+	for i in range(3993):
+		matrix_mean = matrix_mean + matrix[i,:,:]
+	print(np.max(matrix_mean))
+	for j in range(128):
+		for k in range(128):
+			matrix_mean[j,k] = int(matrix_mean[j,k]/3993)
+	#matrix_mean = matrix_mean/3993
+	print(np.max(matrix_mean))
+	return matrix_mean
 
 if __name__ == "__main__":
-	listFiles = ls()
-	for i in range(len(listFiles)):
-		read_img(listFiles[i])
+	#save_imges(ls('rawdata'))
+	images = vector_img(ls('data'))
+	img_mean = mean(images)
+	print(img_mean.shape)
+	cv2.imshow('img_mean',img_mean)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
