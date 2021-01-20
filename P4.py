6613d5f96bea881113bd8e22bc76cb289ac4ce84
CharlieML <charls_brian@hotmail.es>
2021-01-20 03:23:17 -0600

    Se ha agregado conversion de datos crudos a imagenes .png

--- P4.py
+++ P4.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from os import listdir
+from os.path import isfile, join
 
-def read_img():
-    binaryFile = open("rawdata/rawdata/1223","rb")
-    data = np.fromfile(binaryFile, dtype='uint8') # reads the whole file
-    binaryFile.close()
-
-    print(data.shape)
-    data_reshaped = data.reshape(128,128)
-    plt.imshow(data_reshaped)
-    plt.show()
-
+def read_img(fileName):
+	binaryFile = open('rawdata/'+fileName) # Lecura del archivo
+	data = np.fromfile(binaryFile, dtype='uint8') # Conversion a enteros de 8 bits sin signo
+	binaryFile.close()
+	tam = int(np.sqrt(len(data))) # Se optiene el tamaño del la fila y columna
+	data_reshaped = data.reshape(tam,tam) # Se cambia la dimension del vector
+	plt.imsave('data/'+fileName+'.png',data_reshaped, cmap = 'gray') #
+	
+def ls(ruta = 'rawdata'):
+    return [arch for arch in listdir(ruta) if isfile(join(ruta, arch))] #Lista los archivos del directorio	
 
 if __name__ == "__main__":
-    read_img()
+	listFiles = ls()
+	for i in range(len(listFiles)):
+		read_img(listFiles[i])
